576ac6d6dffe56f110adb45ce82cad8d1bd9fd45
Adam Kapos <adam@yousician.com>
2022-10-25 15:25:34 +0300

    Create Lokalise target branch when merging
    In case it doesn't exist. Merging to master/main results in strings landing too early.

--- close-branch/close_branch.sh
+++ close-branch/close_branch.sh
@@ -24,9 +24,8 @@ if [ "$SOURCE_BRANCH" = "master" ]; then
 fi
 
 print_green "Getting ${SOURCE_BRANCH} branch..."
-SOURCE_BRANCH_ID=$(curl --request GET --fail \
-  --url https://api.lokalise.com/api2/projects/${LOKALISE_PROJECT}/branches \
-  --header "x-api-token: $LOKALISE_TOKEN" | jq " .branches[] | select(.name == \"$SOURCE_BRANCH\") | .branch_id")
+SOURCE_BRANCH_ID=$(lokalise2 branch list --project-id "$LOKALISE_PROJECT" --token "$LOKALISE_TOKEN" |
+    jq " .branches[] | select(.name == \"$SOURCE_BRANCH\") | .branch_id")
 
 if [ -z "$SOURCE_BRANCH_ID" ]; then
     print_magenta "Branch $SOURCE_BRANCH does not exist on Lokalise for this project. Nothing to do."
@@ -36,18 +35,17 @@ fi
 if [ "$MERGED" = "true" ]; then
     # If the branch was merged then we find the target branch and merge into it
     print_green "Getting ${TARGET_BRANCH} branch..."
-    TARGET_BRANCH_ID=$(curl --request GET --fail \
-    --url https://api.lokalise.com/api2/projects/${LOKALISE_PROJECT}/branches \
-    --header "x-api-token: $LOKALISE_TOKEN" | jq " .branches[] | select(.name == \"$TARGET_BRANCH\") | .branch_id")
+    TARGET_BRANCH_ID=$(lokalise2 branch list --project-id "$LOKALISE_PROJECT" --token "$LOKALISE_TOKEN" |
+        jq " .branches[] | select(.name == \"$TARGET_BRANCH\") | .branch_id")
 
     if [ -z "$TARGET_BRANCH_ID" ]; then
-        print_yellow "Branch $TARGET_BRANCH does not exist on Lokalise for this project. Using default branch as target."
-        MERGE_PAYLOAD="{\"force_conflict_resolve_using\":\"target\"}"
-        print_green "Merging ${SOURCE_BRANCH} into default branch..."
-    else
-        MERGE_PAYLOAD="{\"force_conflict_resolve_using\":\"target\", \"target_branch_id\": $TARGET_BRANCH_ID}"
-        print_green "Merging ${SOURCE_BRANCH} into ${TARGET_BRANCH}..."
+        print_yellow "Branch $TARGET_BRANCH does not exist on Lokalise for this project. Creating it."
+        TARGET_BRANCH_ID=$(lokalise2 branch create --name "$TARGET_BRANCH" --project-id "$LOKALISE_PROJECT" --token "$LOKALISE_TOKEN" |
+            jq .branch.branch_id)
     fi
+
+    MERGE_PAYLOAD="{\"force_conflict_resolve_using\":\"target\", \"target_branch_id\": $TARGET_BRANCH_ID}"
+    print_green "Merging ${SOURCE_BRANCH} into ${TARGET_BRANCH}..."
 
     curl --request POST --fail \
         --url https://api.lokalise.com/api2/projects/${LOKALISE_PROJECT}/branches/$SOURCE_BRANCH_ID/merge \
@@ -58,6 +56,4 @@ fi
 
 # Finally we delete the branch, even if it was not merged
 print_green "Deleting ${SOURCE_BRANCH}..."
-curl --request DELETE --fail \
-    --url https://api.lokalise.com/api2/projects/${LOKALISE_PROJECT}/branches/$SOURCE_BRANCH_ID \
-    --header "x-api-token: $LOKALISE_TOKEN"
+lokalise2 branch delete --branch-id "$SOURCE_BRANCH_ID" --project-id "$LOKALISE_PROJECT" --token "$LOKALISE_TOKEN"
